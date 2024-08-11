9398259a81e2c03fd33b1b91a1eb68992be25236
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-08-11 10:20:05 -0700

    Define a Protocol for the API Service, Create the Real API Service

---
+++ MockAPI/Model/FaxResponse.swift
@@ -0,0 +1,22 @@
+//
+//  FaxResponse.swift
+//  MockAPI
+//
+//  Created by Tatsuya Moriguchi on 8/11/24.
+//
+
+import Foundation
+//import Combine
+
+// Define a protocol for the Fax API service
+protocol FaxAPIServiceProtocol {
+//    func sendFax(toNumber: String, fileData: Data, fileType: String) -> AnyPublisher<FaxResponse, Error> // Combine
+    func sendFax(toNumber: String, fileData: Data, fileType: String) async throws -> FaxResponse // Async/Await
+}
+
+// Define a model for the reponse.
+struct FaxResponse: Codable {
+    let status: String
+    let faxId: String?
+    let message: String?
+}
---
+++ MockAPI/ViewModel/FaxAPIService.swift
@@ -0,0 +1,56 @@
+//
+//  FaxAPIService.swift
+//  MockAPI
+//
+//  Created by Tatsuya Moriguchi on 8/11/24.
+//
+
+import Foundation
+//import Combine
+
+class FaxAPIService: FaxAPIServiceProtocol {
+    // Dummy url
+    private let baseURL = "https://restapi.efaxcorporate.com/v1.0/faxes"
+    
+//    func sendFax(toNumber: String, fileData: Data, fileType: String) -> AnyPublisher<FaxResponse, any Error> { // Comobine
+    func sendFax(toNumber: String, fileData: Data, fileType: String) async throws -> FaxResponse {  // Async/Await
+
+        guard let url = URL(string: baseURL) else {
+//            return Fail(error: URLError(.badURL)).eraseToAnyPublisher() // Combine
+            throw URLError(.badURL) // Async/Await
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Bearer YOUT_API_TOKEN", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body = [
+        "to": toNumber,
+        "fileData": fileData.base64EncodedString(),
+        "fileType": fileType
+        ]
+
+        // Combine
+//        do {
+//            request.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+//        } catch {
+//            return Fail(error: error).eraseToAnyPublisher()
+//        }
+
+        // Async/Await
+        request.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+        
+
+        // Combine
+//        return URLSession.shared.dataTaskPublisher(for: request)
+//            .map(\.data)
+//            .decode(type: FaxResponse.self, decoder: JSONDecoder())
+//            .eraseToAnyPublisher()
+
+        // Async/Await
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONDecoder().decode(FaxResponse.self, from: data)
+    }
+}
+
