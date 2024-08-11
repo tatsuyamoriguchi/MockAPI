fa7d84c0d77d68ff6a23b5f2bd2313d83f23bf4b
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-08-11 09:18:32 -0700

    Initial Commit

---
+++ MockAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MockAPI
+//
+//  Created by Tatsuya Moriguchi on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockAPI/MockAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockAPIApp.swift
+//  MockAPI
+//
+//  Created by Tatsuya Moriguchi on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MockAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
